da0de0c773fe509aa27c2fdd762b2ab8a2923f87
pkellner <peter@peterkellner.net>
2017-03-30 16:40:56 -0700

    First Checkin With Problem Showing on Home Page

---
+++ WebAppTH1/AutoPriceTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebAppTH1
+{
+    //[HtmlTargetElement("*")]
+    //[HtmlTargetElement("auto-price", Attributes = "model")] // works, must have model
+    //[HtmlTargetElement("auto-price",Attributes ="make")] // works, must have make
+    //[HtmlTargetElement("auto-price", Attributes = "make,model")] // works, must have make and model
+    //[HtmlTargetElement("auto-price", Attributes = "[make ^='p']")] // works, make starts with p
+    //[HtmlTargetElement("auto-price", Attributes = "[model $='r']")] // works, make starts with p
+    [HtmlTargetElement("auto-price", Attributes = "[make ^='p'],[model $= 'r'")] // fails, shows all
+    public class AutoPriceTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
